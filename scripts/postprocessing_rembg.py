7fb6a263d66c4167f386bf6f66b12f187d44505e
Vladimir Mandic <mandic00@live.com>
2023-10-14 12:48:00 -0400

    update

--- scripts/postprocessing_rembg.py
+++ scripts/postprocessing_rembg.py
@@ -7,13 +7,14 @@ from modules.ui_components import FormRow
 
 models = [
     "None",
-    "u2net",
-    "u2netp",
-    "u2net_human_seg",
-    "u2net_cloth_seg",
     "silueta",
+    "u2net",
+    # "u2netp",
+    "u2net_human_seg",
+    # "u2net_cloth_seg",
     "isnet-general-use",
     "isnet-anime",
+    # "sam",
 ]
 
 class ScriptPostprocessingUpscale(scripts_postprocessing.ScriptPostprocessing):
@@ -23,45 +24,47 @@ class ScriptPostprocessingUpscale(scripts_postprocessing.ScriptPostprocessing):
 
     def ui(self):
         with FormRow():
-            model = gr.Dropdown(label="Remove background", choices=models, value="None")
-            return_mask = gr.Checkbox(label="Return mask", value=False)
-            alpha_matting = gr.Checkbox(label="Alpha matting", value=False)
-
-        with FormRow(visible=False) as alpha_mask_row:
+            model = gr.Dropdown(label="Model", choices=models, value="None")
+            only_mask = gr.Checkbox(label="Return mask", value=False)
+            postprocess_mask = gr.Checkbox(label="Postprocess mask", value=False)
+            alpha_matting = gr.Checkbox(label="Alpha matting", value=True)
+        with FormRow(visible=True) as alpha_mask_row:
             alpha_matting_erode_size = gr.Slider(label="Erode size", minimum=0, maximum=40, step=1, value=10)
             alpha_matting_foreground_threshold = gr.Slider(label="Foreground threshold", minimum=0, maximum=255, step=1, value=240)
             alpha_matting_background_threshold = gr.Slider(label="Background threshold", minimum=0, maximum=255, step=1, value=10)
-
-        alpha_matting.change(
-            fn=lambda x: gr.update(visible=x),
-            inputs=[alpha_matting],
-            outputs=[alpha_mask_row],
-        )
-
+        alpha_matting.change(fn=lambda x: gr.update(visible=x), inputs=[alpha_matting], outputs=[alpha_mask_row])
         return {
             "model": model,
-            "return_mask": return_mask,
+            "only_mask": only_mask,
+            "postprocess_mask": postprocess_mask,
             "alpha_matting": alpha_matting,
             "alpha_matting_foreground_threshold": alpha_matting_foreground_threshold,
             "alpha_matting_background_threshold": alpha_matting_background_threshold,
             "alpha_matting_erode_size": alpha_matting_erode_size,
         }
 
-    def process(self, pp: scripts_postprocessing.PostprocessedImage, model, return_mask, alpha_matting, alpha_matting_foreground_threshold, alpha_matting_background_threshold, alpha_matting_erode_size):
+    def process(self, pp: scripts_postprocessing.PostprocessedImage, model, only_mask, postprocess_mask, alpha_matting, alpha_matting_foreground_threshold, alpha_matting_background_threshold, alpha_matting_erode_size): # pylint: disable=arguments-differ
         if not model or model == "None":
             return
-
         if "U2NET_HOME" not in os.environ:
-            os.environ["U2NET_HOME"] = os.path.join(models_path, "u2net")
-
-        pp.image = rembg.remove(
-            pp.image,
-            session=rembg.new_session(model),
-            only_mask=return_mask,
-            alpha_matting=alpha_matting,
-            alpha_matting_foreground_threshold=alpha_matting_foreground_threshold,
-            alpha_matting_background_threshold=alpha_matting_background_threshold,
-            alpha_matting_erode_size=alpha_matting_erode_size,
-        )
-
+            os.environ["U2NET_HOME"] = os.path.join(models_path, "rembg")
+        args = {
+            'data': pp.image,
+            'only_mask': only_mask,
+            'post_process_mask': postprocess_mask,
+            'bgcolor': None,
+            'alpha_matting': alpha_matting,
+            'alpha_matting_foreground_threshold': alpha_matting_foreground_threshold,
+            'alpha_matting_background_threshold': alpha_matting_background_threshold,
+            'alpha_matting_erode_size': alpha_matting_erode_size,
+            'session': rembg.new_session(model),
+        }
+        """
+        # sam is more of a segment-anything model as it needs set of labels an points
+        if model == 'sam':
+            import numpy as np
+            args['input_points'] = np.array([[400, 350], [700, 400], [200, 400]]) # array of y,x
+            args['input_labels'] = np.array([1, 1, 2])
+        """
+        pp.image = rembg.remove(**args)
         pp.info["Rembg"] = model

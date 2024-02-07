4d6b4fd70b00f0ffb4a66a381c85e74e88e04752
Vladimir Mandic <mandic00@live.com>
2024-02-07 09:29:05 -0500

    cleanup

--- install.py
+++ install.py
@@ -3,5 +3,5 @@ import installer
 
 dependencies = ['onnxruntime', 'pymatting', 'pooch']
 for dependency in dependencies:
-    if not installer.installed(dependency, quiet=True):
+    if not installer.installed(dependency):
         installer.install(dependency, ignore=False)

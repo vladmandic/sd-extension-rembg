067b227de15b69ff3fc12f9d1a2b1af2afe7ecdc
Vladimir Mandic <mandic00@live.com>
2025-02-03 09:08:24 -0500

    add ben2
    Signed-off-by: Vladimir Mandic <mandic00@live.com>

--- scripts/postprocessing_rembg.py
+++ scripts/postprocessing_rembg.py
@@ -1,12 +1,15 @@
 import os
 import gradio as gr
+from PIL import Image
 from modules import scripts_postprocessing
 from modules.paths import models_path
 from modules import shared
+import ben2 # pylint: disable=wrong-import-order
 
 
 models = [
-    "None",
+    "none",
+    "ben2",
     "silueta",
     "u2net",
     "u2net_human_seg",
@@ -18,33 +21,40 @@ models = [
 ]
 
 
-def dependencies():
-    import installer
-    for pkg in ['pymatting', 'pooch', 'rembg']:
-        if not installer.installed(pkg):
-            installer.install(pkg, ignore=False)
-
-
 class ScriptPostprocessingUpscale(scripts_postprocessing.ScriptPostprocessing):
     name = "Remove background"
     order = 20000
     model = None
 
     def ui(self):
-        with gr.Accordion('Remove background', open = False):
+        def visible(model):
+            return gr.update(visible=model not in ["none"]), gr.update(visible=model in ["ben2"]), gr.update(visible=model not in ["none", "ben2"])
+
+        with gr.Accordion('Remove background', open = True):
             with gr.Row():
-                model = gr.Dropdown(label="Model", choices=models, value="None", elem_id="extras_rembg_model")
-                only_mask = gr.Checkbox(label="Return mask", value=False, elem_id="extras_rembg_only_mask")
-                postprocess_mask = gr.Checkbox(label="Postprocess mask", value=False, elem_id="extras_rembg_process_mask")
-                alpha_matting = gr.Checkbox(label="Alpha matting", value=True, elem_id="extras_rembg_alpha")
-            with gr.Row(visible=True) as alpha_mask_row:
-                alpha_matting_erode_size = gr.Slider(label="Erode size", minimum=0, maximum=40, step=1, value=10, elem_id="extras_rembg_alpha_erode")
-                alpha_matting_foreground_threshold = gr.Slider(label="Foreground threshold", minimum=0, maximum=255, step=1, value=240, elem_id="extras_rembg_alpha_foreground")
-                alpha_matting_background_threshold = gr.Slider(label="Background threshold", minimum=0, maximum=255, step=1, value=10, elem_id="extras_rembg_alpha_background")
-            alpha_matting.change(fn=lambda x: gr.update(visible=x), inputs=[alpha_matting], outputs=[alpha_mask_row])
+                model = gr.Dropdown(label="Model", choices=models, value="none", elem_id="extras_rembg_model")
+            with gr.Group(visible=False) as group_base:
+                with gr.Row():
+                    merge_alpha = gr.Checkbox(label="Merge alpha", value=False)
+                    mask_only = gr.Checkbox(label="Mask only", value=False)
+            with gr.Group(visible=False) as group_ben2:
+                with gr.Row():
+                    refine = gr.Checkbox(label="Refine foreground", value=False)
+            with gr.Group(visible=False) as group_rembg:
+                with gr.Row():
+                    postprocess_mask = gr.Checkbox(label="Postprocess mask", value=False, elem_id="extras_rembg_process_mask")
+                    alpha_matting = gr.Checkbox(label="Alpha matting", value=False, elem_id="extras_rembg_alpha")
+                with gr.Row(visible=True) as alpha_mask_row:
+                    alpha_matting_erode_size = gr.Slider(label="Erode size", minimum=0, maximum=40, step=1, value=10, elem_id="extras_rembg_alpha_erode")
+                    alpha_matting_foreground_threshold = gr.Slider(label="Foreground threshold", minimum=0, maximum=255, step=1, value=240, elem_id="extras_rembg_alpha_foreground")
+                    alpha_matting_background_threshold = gr.Slider(label="Background threshold", minimum=0, maximum=255, step=1, value=10, elem_id="extras_rembg_alpha_background")
+                alpha_matting.change(fn=lambda x: gr.update(visible=x), inputs=[alpha_matting], outputs=[alpha_mask_row])
+            model.change(fn=lambda x: visible(x), inputs=[model], outputs=[group_base, group_ben2, group_rembg])
             return {
                 "model": model,
-                "only_mask": only_mask,
+                "merge_alpha": merge_alpha,
+                "refine": refine,
+                "mask_only": mask_only,
                 "postprocess_mask": postprocess_mask,
                 "alpha_matting": alpha_matting,
                 "alpha_matting_foreground_threshold": alpha_matting_foreground_threshold,
@@ -52,34 +62,56 @@ class ScriptPostprocessingUpscale(scripts_postprocessing.ScriptPostprocessing):
                 "alpha_matting_erode_size": alpha_matting_erode_size,
             }
 
-    def process(self, pp: scripts_postprocessing.PostprocessedImage, model, only_mask, postprocess_mask, alpha_matting, alpha_matting_foreground_threshold, alpha_matting_background_threshold, alpha_matting_erode_size): # pylint: disable=arguments-differ
-        dependencies()
-        try:
-            import rembg
-        except Exception as e:
-            shared.log.error(f'Rembg load failed: {e}')
-            return
-        if not model or model == "None":
-            return
-        if "U2NET_HOME" not in os.environ:
-            os.environ["U2NET_HOME"] = os.path.join(models_path, "Rembg")
-        args = {
-            'data': pp.image,
-            'only_mask': only_mask,
-            'post_process_mask': postprocess_mask,
-            'bgcolor': None,
-            'alpha_matting': alpha_matting,
-            'alpha_matting_foreground_threshold': alpha_matting_foreground_threshold,
-            'alpha_matting_background_threshold': alpha_matting_background_threshold,
-            'alpha_matting_erode_size': alpha_matting_erode_size,
-            'session': rembg.new_session(model),
-        }
-        """
-        # sam is more of a segment-anything model as it needs set of labels an points
-        if model == 'sam':
-            import numpy as np
-            args['input_points'] = np.array([[400, 350], [700, 400], [200, 400]]) # array of y,x
-            args['input_labels'] = np.array([1, 1, 2])
-        """
-        pp.image = rembg.remove(**args)
-        pp.info["Rembg"] = model
+    def process(self, pp: scripts_postprocessing.PostprocessedImage, model, merge_alpha, refine, mask_only, postprocess_mask, alpha_matting, alpha_matting_foreground_threshold, alpha_matting_background_threshold, alpha_matting_erode_size): # pylint: disable=arguments-differ
+        if not model or model == "none":
+            return pp
+        if isinstance(pp, Image.Image):
+            image = pp
+            info = {}
+        else:
+            image = pp.image
+            info = pp.info
+
+        if model == 'ben2':
+            try:
+                image = ben2.remove(image, refine=refine)
+            except Exception as e:
+                shared.log.error(f'RemoveBackground: model={model} {e}')
+                return pp
+        else:
+            try:
+                import installer
+                for pkg in ['pymatting', 'pooch', 'rembg']:
+                    installer.install(pkg, ignore=False)
+                import rembg
+                if "U2NET_HOME" not in os.environ:
+                    os.environ["U2NET_HOME"] = os.path.join(models_path, "Rembg")
+                image = rembg.remove(image,
+                                    post_process_mask=postprocess_mask,
+                                    alpha_matting=alpha_matting,
+                                    alpha_matting_foreground_threshold=alpha_matting_foreground_threshold,
+                                    alpha_matting_background_threshold=alpha_matting_background_threshold,
+                                    alpha_matting_erode_size=alpha_matting_erode_size,
+                                    session=rembg.new_session(model))
+            except Exception as e:
+                shared.log.error(f'RemoveBackground: model={model} {e}')
+                return pp
+
+        if mask_only and pp.image.mode == "RGBA":
+            _r, _g, _b, alpha = pp.image.split()
+            image = alpha
+        if merge_alpha and pp.image.mode == "RGBA":
+            flattened = Image.new("RGBA", pp.image.size, "BLACK")
+            flattened.paste(pp.image, mask=pp.image)
+            flattened.convert("RGB")
+            image = flattened
+
+        if isinstance(pp, Image.Image):
+            pp = scripts_postprocessing.PostprocessedImage(
+                image=image,
+                info={ **info, 'Rembg': model },
+            )
+        else:
+            pp.image = image
+            pp.info['Rembg'] = model
+        return pp
